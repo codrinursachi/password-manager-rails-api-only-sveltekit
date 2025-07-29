8bcc7fe13ce9a70e9830db230732e298658f9200
codrinursachi <codrinursachi@gmail.com>
2025-07-29 18:09:27 +0300

    update loading ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,17 @@
 	import { afterNavigate, beforeNavigate } from '$app/navigation';
 
 	BProgress.configure({ showSpinner: false });
+
 	beforeNavigate(() => {
 		BProgress.start();
+		document.querySelector('#overlay')?.classList.add('opacity-50');
+		document.querySelector('#loading')?.classList.toggle('hidden');
 	});
 
 	afterNavigate(() => {
 		BProgress.done();
+		document.querySelector('#loading')?.classList.toggle('hidden');
+		document.querySelector('#overlay')?.classList.remove('opacity-50');
 	});
 
 	let { children } = $props();
@@ -22,6 +27,12 @@
 	<title>Password Manager</title>
 </svelte:head>
 
-<QueryClientProvider client={queryClient}>
-	{@render children()}
-</QueryClientProvider>
+<div
+	id="loading"
+	class="size-25 outline-8-black absolute left-1/2 top-1/2 z-50 hidden -translate-x-1/2 -translate-y-1/2 animate-spin rounded-full border-8 border-gray-200 border-t-black"
+></div>
+<div id="overlay">
+	<QueryClientProvider client={queryClient}>
+		{@render children()}
+	</QueryClientProvider>
+</div>
